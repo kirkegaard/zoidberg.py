380b8daa4889acd257bf0d89c124c2e11cde213c
Christian Kirkegaard <hello@christiank.org>
2019-03-08 12:57:06 +0100

    Add response to utilities

--- zoidberg/plugins/utilities.py
+++ zoidberg/plugins/utilities.py
@@ -5,12 +5,15 @@ class Utilities():
 
     def load(self, context, plugin):
         self.bot.load_plugin(plugin)
+        context.send('Plugin loaded')
 
     def reload(self, context, plugin):
         self.bot.reload_plugin(plugin)
+        context.send('Plugin reloaded')
 
     def unload(self, context, plugin):
         self.bot.unload_plugin(plugin)
+        context.send('Plugin unloaded')
 
 
 def setup(bot):
